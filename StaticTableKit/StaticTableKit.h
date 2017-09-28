089ffb68139f68cef55c624f9d32803b9a9ee4de
Shane Whitehead <obishiro@gmail.com>
2017-09-29 07:48:26 +1000

    Initial Commit

---
+++ StaticTableKit/StaticTableKit.h
@@ -0,0 +1,19 @@
+//
+//  StaticTableKit.h
+//  StaticTableKit
+//
+//  Created by Shane Whitehead on 29/9/17.
+//  Copyright © 2017 Shane Whitehead. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StaticTableKit.
+FOUNDATION_EXPORT double StaticTableKitVersionNumber;
+
+//! Project version string for StaticTableKit.
+FOUNDATION_EXPORT const unsigned char StaticTableKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StaticTableKit/PublicHeader.h>
+
+
