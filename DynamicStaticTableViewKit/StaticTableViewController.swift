e0b02347b5595b837cec65f778c547d4923d9aa7
Shane Whitehead <obishiro@gmail.com>
2017-09-30 08:52:52 +1000

    Rename core project

--- DynamicStaticTableViewKit/StaticTableViewController.swift
+++ DynamicStaticTableViewKit/StaticTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-open class staticTableViewModel: UITableViewController {
+open class DynamicStaticTableViewController: UITableViewController {
   
   // MARK: Custom properties
   
